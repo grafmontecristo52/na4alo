3048f48e3483a1b5b7045dd6cba277f0ae328352
cristoKali <cristoKali@gmail.com>
2026-05-01 14:34:29 -0400

    some easy C code

---
+++ C/c1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+printf("Hello, World!\n");
+printf("The world is yours!\n");
+	return 0;
+}
---
+++ C/c3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{printf("NICKNAME: Neo");
+	printf("CITY: St-Petersburg");
+	printf("AGE: 35");
+	printf("HEIGHT: 180");
+	printf("WEIGHT: 75");
+	return 0;
+}
+
