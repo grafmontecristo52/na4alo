3dee7d936ae3ad5e0ba7115acb22c4dbb2899980
cristoKali <cristoKali@gmail.com>
2026-05-04 08:54:06 -0400

    filtred

---
+++ C/ccat/c5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+int ystalost6 = 90;
+	printf("Negri pidorasis1234");
+printf("Ya xo4y vizhit6");
+printf("Mne tyjaleo, no ya spravlys6");
+printf("%d", ystalost6);
+	return 0;
+}
