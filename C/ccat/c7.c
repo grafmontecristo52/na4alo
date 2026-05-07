1d2fc627068bccd7c367377e67a119a76b131e04
spb <ubuntu@gmaile.com>
2026-05-07 21:49:43 +0300

    some basic

--- C/ccat/c7.c
+++ C/ccat/c7.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 int main()
 {
-printf("Я в ахуях тотальных сука");
+char *name = "Vanya";
+int age = 19;
+char *favorite_lan = "C";
+printf("My name %s, im %d years old and my favorite lang is %s!\n", name,age,favorite_lan);
 return 0;
 }
-
