6faaadefd26478ae863b34ff5c897d9d0645b62a
spb <ubuntu@gmaile.com>
2026-05-11 20:30:04 +0300

    Небольшой код по доте!

---
+++ C/ccat/c10.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	int arr[] = {5, 6, 3, 4, 1};	
+	printf("%d\n", arr[2]);
+	printf("%d\n", arr[0]);
+	printf("%d\n", arr[4]);
+	float num[3];
+	num[0] = 5.4f;	
+	num[1] = 5.3f;
+	num[2] = 5.2f;
+	num[3] = 5.1f;
+	printf("%f\n", num[3]);
+	char im[] = {"Негры пидарасы"};
+	printf("%s\n", im);
+	return 0;
+}
